7bb6f8981fec5738817a6ef4d3566620d3f44bb0
claudionhangapc <claudionhangapc@gmail.com>
2021-12-30 05:03:10 -0300

    creat dockerfile

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 const server = require('./src/app')
 
-const port = process.env.Port ||  3000
-
+const port = process.env.Port || 3000
+const host = process.env.HOST || '0.0.0.0';
 const start = async ()=>{
   try{
-    await server.listen(port)
+    await server.listen({port,host})
   }catch(err){
     server.log.error(err)
     process.exit(1)
