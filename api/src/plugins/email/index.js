678f12a70bf71d07ec2940b417f55e0575f55483
claudionhangapc <claudionhangapc@gmail.com>
2022-02-11 20:10:42 -0300

    register transport plugin

--- api/src/plugins/email/index.js
+++ api/src/plugins/email/index.js
@@ -1,11 +1,20 @@
+require('dotenv').config()
 const fastifyPlugin = require('fastify-plugin')
 
 async function sendEmail (fastify, options) {
   /*
-  * registra as chaves
-  * do certificado
+  * registra o transporter
   */
-  
+  fastify.register(require('fastify-nodemailer'), {
+    name: process.env.TRANSPORTER_NAME,
+    host: process.env.TRANSPORTER_HOST,
+    port: process.env.TRANSPORTER_PORT,
+    secure: true, // use TLS
+    auth: {
+        user: process.env.TRANSPORTER_AUTH_USER,
+        pass: process.env.TRANSPORTER_AUTH_PASS
+    }
+  })
 }
 
 module.exports = fastifyPlugin(sendEmail)
