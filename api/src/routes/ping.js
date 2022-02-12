4b01749b30c1996dd44e69019bf64b3990b9627c
claudionhangapc <claudionhangapc@gmail.com>
2022-02-12 18:51:34 -0300

    ajust ping route

--- api/src/routes/ping.js
+++ api/src/routes/ping.js
@@ -1,14 +1,9 @@
-const email = require('../services/email')
 
 module.exports = function (fastify, option, done) {
-  const emailInsatance = new email(fastify)
   /*
    * ping
    */
   fastify.get('/', async (request, reply) => {
-    //const result = await emailInsatance.signIn()
-    //console.log(result)
-    //reply.send(result)
    reply.send({
       ping: 'pong'
     })
