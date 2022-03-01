aa25758580bcb8cdcfb6bff8eb0cc362b2b0c7fe
claudionhangapc <claudionhangapc@gmail.com>
2022-02-28 21:14:22 -0300

    added all mutations  - store - tasks - app

--- app/store/task.js
+++ app/store/task.js
@@ -16,6 +16,23 @@ export const actions = {
 export const mutations = {
 
   ADD(state,payload){
+    state.tasks.push(payload)
+  },
+
+  SET(state, payload){
     state.tasks = payload
   },
+
+  DELETE(state, id){
+    const index = state.tasks.findIndex(naver => naver.id ===id)
+    state.tasks.splice(index,1)
+  },
+
+  UPDATE_SINGLE(state, payload){
+    const index = state.tasks.findIndex(task => task.id ===payload.id)
+    if (index !== -1) {
+      state.tasks.splice(index,1,payload)
+    }
+    
+  }
 }
