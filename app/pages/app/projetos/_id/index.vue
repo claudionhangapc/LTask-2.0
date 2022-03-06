f1568c165f5bef8994dc6673cfcb533de2f61c7d
claudionhangapc <claudionhangapc@gmail.com>
2022-03-06 20:08:41 -0300

    add folder tarefas to projects - app

---
+++ app/pages/app/projetos/_id/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    ola testando mais uma vez
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      
+    }
+  },
+  mounted() {
+    //this.$router.replace('/app/projetos');
+  },
+ 
+}
+</script>
---
+++ app/pages/app/projetos/_id/tarefas.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Agora estas em tarefas
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      
+    }
+  },
+  mounted() {
+    //this.$router.replace('/app/projetos');
+  },
+ 
+}
+</script>
