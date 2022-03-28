76f9cdb5a9899811e34c85d62e1771f30b4be639
claudionhangapc <claudionhangapc@gmail.com>
2022-03-28 00:59:10 -0300

    fetched projet in projects/_id page - app

--- app/pages/app/projetos/_id/index.vue
+++ app/pages/app/projetos/_id/index.vue
@@ -1,19 +1,50 @@
 <template>
-  <div>
-    ola testando mais uma vez
-  </div>
+  <v-row>
+       <v-col 
+         cols="12"
+         class="my-0 d-flex align-center"
+         >
+         <v-icon >
+          mdi-folder-outline
+        </v-icon>
+        <h3 class="ml-2">  
+          {{this.project.name}}  
+        </h3>
+      </v-col>
+      <v-col
+         cols="12"
+         class="py-0"
+         >
+         <project :project="this.project" />
+      </v-col>
+   </v-row>
 </template>
 
 <script>
 export default {
   data(){
     return{
-      
+      project:{
+
+      }
     }
+  },
+  created(){
+     this.fetchProject()
   },
   mounted() {
     //this.$router.replace('/app/projetos');
   },
+  computed:{
+    id(){
+      return this.$route.params.id
+      },   
+  },
+  methods:{
+    async fetchProject(){
+      this.project = await this.$store.dispatch('project/fetchSingle', this.id)
+    }
+  }
  
 }
 </script>
