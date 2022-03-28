c063dbececbd8e42917c31df3791909d42c985eb
claudionhangapc <claudionhangapc@gmail.com>
2022-03-27 23:40:48 -0300

    added content to page task/important - app

--- app/pages/app/tarefas/importantes.vue
+++ app/pages/app/tarefas/importantes.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    estas na pagina importantes
-  </div>
+  <v-row>
+       <v-col 
+         cols="12"
+         class="my-0 d-flex align-center"
+         >
+         <v-checkbox
+              class="mt-0 pt-0"
+              hide-details
+              :on-icon="'mdi-star'"
+              :off-icon="'mdi-star-outline'"
+          ></v-checkbox>
+         <h3 >  
+          Importantes             
+        </h3>
+      </v-col>
+      <v-col
+         cols="12"
+         class="py-0"
+         >
+         <tasks :tasks="tasks"/>
+      </v-col>
+   </v-row>
 </template>
 
 <script>
@@ -11,6 +30,11 @@ export default {
       
     }
   },
+  computed:{
+      tasks(){
+        return this.$store.state.task.tasks
+      }
+    },
  
 }
 </script>
