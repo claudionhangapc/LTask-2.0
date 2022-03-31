df3428b43056924d9d9c42f5230d426a249e8277
claudionhangapc <claudionhangapc@gmail.com>
2022-03-31 19:06:50 -0300

    added category edit page - app

---
+++ app/pages/app/categorias/index/editar/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+   <ModalCategoryAdd :dialog="dialogCategory" @closeModalCategory="updatedialogCategory($event)"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      dialogCategory:true,
+    }
+  },
+  methods:{
+    updatedialogCategory(value){
+        this.dialogCategory = value
+    },
+  }
+ 
+}
+</script>
