5fbcc3064dc65eb5f3491fe55b8b19bd4880973b
claudionhangapc <claudionhangapc@gmail.com>
2022-03-31 19:10:00 -0300

    added category edit id  page - app

---
+++ app/pages/app/categorias/index/editar/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ModalProjectEdit :dialog="dialogProject" @closeModalProject="updatedialogProject($event)"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      dialogProject:true,
+    }
+  },
+  methods:{
+    updatedialogProject(value){
+        this.dialogProject = value
+        this.$router.replace('/app/projetos');
+    },
+  }
+}
+</script>
