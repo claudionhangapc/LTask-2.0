7944a392d62236aa41fe564f2800a83ba5ddb282
claudionhangapc <claudionhangapc@gmail.com>
2022-04-09 23:11:51 -0300

    added first option from footer - app

--- app/components/landingpage/TheFooter.vue
+++ app/components/landingpage/TheFooter.vue
@@ -3,25 +3,32 @@
   <section>
    <v-container fluid class="pt-10">
       <v-row align="center" justify="center">
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="5">
           <h4>
-            Criar as suas tarefas 
+            <v-toolbar-title>L<span style="color:#FF8700">Task</span></v-toolbar-title>
           </h4>
-          <h2 class="py-3">
-            Lorem ipsum dolor sit amet, consectetur 
-          </h2>
+          <p class="py-2 my-0">
+            Junta se a dezena de pessoas que tem usado este app para organizar as suas ativadedes
+          </p>
           <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quasi, inventore ipsum repellendus dignissimos aliquam, autem exercitationem, placeat rerum minus natus dolores illo nisi neque facere tempora velit ab laudantium. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed quisquam expedita iure fuga sint rem pariatur soluta repudiandae nulla est ipsam voluptates deserunt perspiciatis unde aliquid error culpa, consequatur similique!
-            </p>
-          </div>
-          <div class="py-2">
-            <v-btn  outlined text>
-              <span >Inicia agora mesmo</span>
-            </v-btn>
+            
+          <v-card flat tile class=" white--text ">
+            <v-card-text class="pa-0">
+              <v-btn
+                v-for="(icon, i) in icons"
+                :key="i"
+                class="mr-4 "
+                :href="icon.link"
+                target="_blank"
+                icon
+              >
+                <v-icon size="24px">{{ icon.text }}</v-icon>
+              </v-btn>
+            </v-card-text>
+          </v-card>
           </div>
         </v-col>
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="7">
             <v-row>
              <v-col cols="12" md="6">
                
@@ -47,7 +54,24 @@
   export default {
     
     data: () => ({
-      
+      icons: [
+      {
+        text: "mdi-facebook",
+        link: "",
+      },
+      {
+        text: "mdi-twitter",
+        link: "",
+      },
+      {
+        text: "mdi-linkedin",
+        link: "",
+      },
+      {
+        text: "mdi-instagram",
+        link: "",
+      },
+    ],
     }),
     
   }
