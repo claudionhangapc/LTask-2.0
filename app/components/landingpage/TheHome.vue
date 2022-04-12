5f63e09884a80681c7b3336c9405d88fc911edab
claudionhangapc <claudionhangapc@gmail.com>
2022-04-11 22:30:12 -0300

    changed image home background - app

--- app/components/landingpage/TheHome.vue
+++ app/components/landingpage/TheHome.vue
@@ -2,23 +2,21 @@
 <template>
   <section >
     <!--  https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg-->
-    <v-parallax  src="bg-black.jpg"  >
+    <v-parallax  src="hand-draw.jpg"  >
       <v-row align="center" justify="center">
         <v-col cols="12" md="7">
-          <h1 class="display-2">
+          <h1 class="display-2" style="color:#333">
             Facilite o gerenciamento das suas atividades
           </h1>
-          <h2 class="display-2 mt-3">
-            Seja mais produtivo
-          </h2>
           <div class="py-8">
-            <p>
+            <p style="color:#333">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione hic molestias neque consequatur corrupti non pariatur repellendus sunt omnis nobis doloremque itaque facilis reiciendis, voluptatem nesciunt! Consequatur sequi ratione nemo!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eaque harum vel velit debitis facilis architecto dolores porro nulla quia. Dolorum rerum dolores autem beatae sed magni fugit sint. Quasi.
           </p>
           </div>
           <div>
-            <v-btn  >
-              <span >Criar uma conta</span>
+            <v-btn  color="#000">
+              <span style="color:#fff">Criar uma conta</span>
             </v-btn>
           </div>
         </v-col>
