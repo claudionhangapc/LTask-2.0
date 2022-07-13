90550ea17c98796017bcf196ad65e9021d57fc7d
claudionhangapc <claudionhangapc@gmail.com>
2022-07-12 23:03:32 -0300

    column img_url removed from seed

--- api/src/database/seeds/01_users.js
+++ api/src/database/seeds/01_users.js
@@ -7,8 +7,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('user').insert([
-        { name:'claudio nhanga', email: 'emaildteste@gmail.com', password: bcrypt.hashSync('12#rer', PASSWORD_SALT), image_url:'https://www.npmjs.com/package/bcrypt', verified:true},
-        { name:'claudio nhanga', email: 'emaildteste2@gmail.com', password: bcrypt.hashSync('12#6565rer', PASSWORD_SALT), image_url:'https://www.npmjs.com/package/bcrypt', verified:true},
+        { name:'claudio nhanga', email: 'emaildteste@gmail.com', password: bcrypt.hashSync('12#rer', PASSWORD_SALT), verified:true},
+        { name:'claudio nhanga', email: 'emaildteste2@gmail.com', password: bcrypt.hashSync('12#6565rer', PASSWORD_SALT), verified:true},
       ]);
     });
 };
